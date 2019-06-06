664db011564f5a8f51ff29487b7a474b7f97ffec
Vitaliy <deinega.vit1@gmail.com>
2019-06-06 13:21:39 +0300

    js.js

---
+++ js.js
@@ -0,0 +1,24 @@
+let i = 0;
+if (i == 0) {
+    i++;
+    console.log(i);
+} else if( i<=0) {
+    i--;
+    console.log(i);
+}else{
+    i--;
+    console.log(i);
+}
+(i == 0) ? console.log(i) : console.log('Не верно');
+
+switch (i) {
+    case i = 0:
+    console.log(i);
+    break;
+    case i = 1:
+    console.log(i);
+    break;
+    default:
+    console.log(i);
+    break;
+}
